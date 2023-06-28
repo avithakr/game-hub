3c43012f2de5e511218362e80c4535bec69f1cf4
Avinash Thakur <thakuravisuman@gmail.com>
2023-06-28 15:38:46 +0530

    displaying critic score

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -5,19 +5,20 @@ import {
   FaApple,
   FaLinux,
   FaAndroid,
-} from "react-icons/fa";
-import { MdPhoneIphone } from "react-icons/md";
-import { SiNintendo } from "react-icons/si";
-import { BsGlobe } from "react-icons/bs";
-import { Icon, HStack } from "@chakra-ui/react";
-import { Platform } from "../hooks/useGames";
-import { IconType } from "react-icons/lib/esm/iconBase";
+} from 'react-icons/fa';
+import { MdPhoneIphone } from 'react-icons/md';
+import { SiNintendo } from 'react-icons/si';
+import { BsGlobe } from 'react-icons/bs';
+import { Icon, HStack } from '@chakra-ui/react';
+import { Platform } from '../hooks/useGames';
+import { IconType } from 'react-icons/lib/esm/iconBase';
 
 interface Props {
   platforms: Platform[];
 }
 
 const PlatformIconList = ({ platforms }: Props) => {
+  // [] this represent the index signature.
   const iconMap: { [key: string]: IconType } = {
     // name: PlayStation
     // slug: playstation
