afa11fd4e63af68305bfb8acebe4f66d086b54e3
Avinash Thakur <thakuravisuman@gmail.com>
2023-07-03 16:16:13 +0530

    Highlite the selected genre

--- src/components/GenresList.tsx
+++ src/components/GenresList.tsx
@@ -12,9 +12,10 @@ import getCroppedImageUrl from '../services/image-url';
 
 interface Props {
   onSelectGenre: (genre: Genre) => void;
+  selectedGenre: Genre | null;
 }
 
-const GenresList = ({ onSelectGenre }: Props) => {
+const GenresList = ({ selectedGenre, onSelectGenre }: Props) => {
   const { data, isLoading, error } = useGenres();
 
   if (error) return null;
@@ -33,6 +34,7 @@ const GenresList = ({ onSelectGenre }: Props) => {
                 borderRadius={8}
               />
               <Button
+                fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'}
                 onClick={() => onSelectGenre(genre)}
                 fontSize="lg"
                 variant="link"
